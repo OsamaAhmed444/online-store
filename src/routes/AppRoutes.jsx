fd7c6090fd116fabe224e5aeace95be2e214b8d6
adel_mhmd77 <am3507962@gmail.com>
2026-09-12 09:25:51 +0300

    project built

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Online Store</div>} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
